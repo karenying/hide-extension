467df11debf7c59970f6a23abdee8e2a6debcd9d
Karen Ying <karenying7@gmail.com>
2020-09-23 09:55:42 -0400

    Refactor and remove LinkedIn home button from navigation

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,5 +1,21 @@
-const main = document.querySelector("#MainContent");
+function hideWeatherFeed() {
+  const main = document.querySelector("#MainContent");
 
-if (main) {
-  main.style.opacity = 0;
+  if (main) {
+    main.style.opacity = 0;
+  }
+}
+
+function hideLinkedinHomeIcon() {
+  const home = document.querySelector(".global-nav__primary-item");
+
+  if (home) {
+    home.remove();
+  }
+}
+
+if (location.href === "https://weather.com/") {
+  hideWeatherFeed();
+} else if (location.href.includes("linkedin")) {
+  hideLinkedinHomeIcon();
 }
